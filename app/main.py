2b6af9bdda97089d5dcda3fe6ff00f9d738f5200
VMAzure <hello@azureautomotive.it>
2025-03-22 14:48:02 +0100

    Id da targa AZLease 2

--- app/main.py
+++ app/main.py
@@ -117,7 +117,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
   
-
 #   allow_credentials=True,
 #     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 #     allow_headers=["Authorization", "Content-Type"]
