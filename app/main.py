f1bad91edd86245c7ea756ab8f22e931bc1ef8a2
VMAzure <hello@azureautomotive.it>
2025-03-24 10:21:31 +0100

    CORS Free all

--- app/main.py
+++ app/main.py
@@ -115,12 +115,12 @@ app.add_middleware(
     allow_origins=["*"],  
     allow_credentials=False,
     allow_methods=["*"],
-    allow_headers=["*"],
-  
+    allow_headers=["*"]
+)  
 #   allow_credentials=True,
 #     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 #     allow_headers=["Authorization", "Content-Type"]
-)
+
 
 
 
