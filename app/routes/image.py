4504abddd919f3bc033b53d67dad4d36b9b3a63c
VMAzure <hello@azureautomotive.it>
2025-04-05 17:28:19 +0200

    rotta immagini 4

--- app/routes/image.py
+++ app/routes/image.py
@@ -23,24 +23,31 @@ async def get_vehicle_image(
     alias = db.query(AzImage).filter(AzImage.codice_modello == codice_modello).first()
 
     if alias:
-        marca = alias.marca_alias
         modello = alias.modello_alias
         model_variant = alias.model_variant
+
+        if alias.marca_alias:
+            marca = alias.marca_alias
+        else:
+            modello_data = db.query(MnetModelli).filter(MnetModelli.codice_modello == codice_modello).first()
+            marca_data = db.query(MnetMarche).filter(MnetMarche.acronimo == modello_data.marca_acronimo).first()
+            marca = marca_data.nome.lower().replace(" ", "-")
     else:
         # Recupero dati originali
         modello_data = db.query(MnetModelli).filter(MnetModelli.codice_modello == codice_modello).first()
-        
+
         if not modello_data:
             raise HTTPException(status_code=404, detail="Modello non trovato.")
 
         marca_data = db.query(MnetMarche).filter(MnetMarche.acronimo == modello_data.marca_acronimo).first()
-        
+
         if not marca_data:
             raise HTTPException(status_code=404, detail="Marca non trovata.")
 
         marca = marca_data.nome.lower().replace(" ", "-")
         modello = modello_data.descrizione.lower().replace(" ", "-")
         model_variant = None
+
 
     # Preparo parametri per la CDN
     params = {
