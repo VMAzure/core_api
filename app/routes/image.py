9de101c1ccca56760964e92f1c275e9ca8918ef4
VMAzure <hello@azureautomotive.it>
2025-04-05 17:47:25 +0200

    rotta immagini 5

--- app/routes/image.py
+++ app/routes/image.py
@@ -1,8 +1,8 @@
-﻿from fastapi import APIRouter, Depends, HTTPException, Query
+﻿from fastapi import APIRouter, Depends, HTTPException, Query, Response
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import AzImage, MnetModelli, MnetMarche, User
-from app.routes.nlt import get_current_user  # 👈 correggi questa linea
+from app.routes.nlt import get_current_user
 import requests
 
 router = APIRouter(
@@ -19,7 +19,6 @@ async def get_vehicle_image(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
-    # Cerco prima alias nella tabella az_image
     alias = db.query(AzImage).filter(AzImage.codice_modello == codice_modello).first()
 
     if alias:
@@ -33,14 +32,11 @@ async def get_vehicle_image(
             marca_data = db.query(MnetMarche).filter(MnetMarche.acronimo == modello_data.marca_acronimo).first()
             marca = marca_data.nome.lower().replace(" ", "-")
     else:
-        # Recupero dati originali
         modello_data = db.query(MnetModelli).filter(MnetModelli.codice_modello == codice_modello).first()
-
         if not modello_data:
             raise HTTPException(status_code=404, detail="Modello non trovato.")
 
         marca_data = db.query(MnetMarche).filter(MnetMarche.acronimo == modello_data.marca_acronimo).first()
-
         if not marca_data:
             raise HTTPException(status_code=404, detail="Marca non trovata.")
 
@@ -48,23 +44,23 @@ async def get_vehicle_image(
         modello = modello_data.descrizione.lower().replace(" ", "-")
         model_variant = None
 
-
-    # Preparo parametri per la CDN
     params = {
         "make": marca,
         "modelFamily": modello,
         "angle": angle,
         "customer": "it-azureautomotive",
-        "billingtag": "core"
+        "billingtag": "core",
+        "zoomlevel": 1,
+        "zoomType": "fullscreen",
+        "randomPaint": "true"
     }
 
     if model_variant:
         params["modelVariant"] = model_variant
 
-    # Formo l'URL finale
     response = requests.get(IMAGIN_CDN_BASE_URL, params=params)
 
     if response.status_code != 200:
         raise HTTPException(status_code=502, detail="Errore CDN Imagin.")
 
-    return {"success": True, "image_url": response.url}
+    return Response(content=response.content, media_type=response.headers['Content-Type'])
