c0d89952b2d1aec2d74c5d5e84f4abd5f36d142b
VMAzure <hello@azureautomotive.it>
2025-04-21 13:24:08 +0200

    prime rotte insurance 8

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ from app.routes.pdf import router as pdf_router
 from app.routes.nlt_offerte import router as nlt_offerte_router
 from app.routes import tools
 from app.routes.image import router as image_router
-from app.routes.azinsurance import preventivi, insurance, setup
+from app.routes.azinsurance import insurance, setup
 
 
 # ✅ Configuriamo il logging
@@ -162,7 +162,6 @@ app.include_router(router_nuovo, prefix="/api")
 app.include_router(tools.router, prefix="/tools", tags=["Tools"])
 app.include_router(image_router)
 
-app.include_router(preventivi.router, prefix="/azinsurance")
 app.include_router(insurance.router, prefix="/azinsurance")
 app.include_router(setup.router, prefix="/azinsurance")
 
