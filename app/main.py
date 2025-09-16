41aef9be132e985e22fa0bb651fefe3c5961fda1
VMAzure <hello@azureautomotive.it>
2025-09-16 08:50:42 +0200

    rotta dall e per combine image 4

--- app/main.py
+++ app/main.py
@@ -61,7 +61,6 @@ from app.routes.videos import router as videos_router
 from app.routes import video_maker
 from app.routes.modelli_ai_test import router as modelli_ai_test_router
 from app.routes import immagini_nuovo
-from app.routes import dalle
 
 
 
@@ -224,7 +223,6 @@ app.include_router(videos_router)
 app.include_router(video_maker.router)
 app.include_router(modelli_ai_test_router)
 app.include_router(immagini_nuovo.router)
-app.include_router(dalle.router)
 
 
 
